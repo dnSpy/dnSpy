981fd8fc7f43a6c857c38bf80af3f118b9b38d27
Daniel Grunwald <daniel@danielgrunwald.de>
2011-02-16 13:42:11 +0100

    Add icon for ILSpy.

--- ILSpy/Images/Images.cs
+++ ILSpy/Images/Images.cs
@@ -21,7 +21,7 @@ namespace ICSharpCode.ILSpy
 		
 		public static readonly BitmapImage Assembly = LoadBitmap("Assembly");
 		public static readonly BitmapImage AssemblyWarning = LoadBitmap("AssemblyWarning");
-		public static readonly BitmapImage AssemblyLoading = LoadBitmap("Open");
+		public static readonly BitmapImage AssemblyLoading = LoadBitmap("FindAssembly");
 		
 		public static readonly BitmapImage Library = LoadBitmap("Library");
 		public static readonly BitmapImage Namespace = LoadBitmap("NameSpace");
