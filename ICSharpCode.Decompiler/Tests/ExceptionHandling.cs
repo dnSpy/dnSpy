9971c664d7efcf1ef5b56244f710775a8a1041d2
Daniel Grunwald <daniel@danielgrunwald.de>
2011-02-23 17:03:28 +0100

    Add tests for method ending with finally block/catch handler.

---
+++ ICSharpCode.Decompiler/Tests/ExceptionHandling.cs
@@ -0,0 +1,25 @@
+﻿// Copyright (c) AlphaSierraPapa for the SharpDevelop Team (for details please see \doc\copyright.txt)
+// This code is distributed under MIT X11 license (for details please see \doc\license.txt)
+
+using System;
+
+public class ExceptionHandling
+{
+	public void MethodEndingWithEndFinally()
+	{
+		try {
+			throw null;
+		} finally {
+			Console.WriteLine();
+		}
+	}
+	
+	public void MethodEndingWithRethrow()
+	{
+		try {
+			throw null;
+		} catch {
+			throw;
+		}
+	}
+}
