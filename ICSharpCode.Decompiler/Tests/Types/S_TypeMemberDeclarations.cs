93a12a9b82c90364f9744fc3622b281e1a23c857
Ed Harvey <edharvey@optusnet.com.au>
2011-04-22 17:31:35 +1000

    Added tests for shadowed events.

--- ICSharpCode.Decompiler/Tests/Types/S_TypeMemberDeclarations.cs
+++ ICSharpCode.Decompiler/Tests/Types/S_TypeMemberDeclarations.cs
@@ -668,4 +668,22 @@ namespace MethodHideGeneric2
 		{
 		}
 	}
+	//$$ EventHiding
+	namespace EventHiding
+	{
+		public class A
+		{
+			public virtual event EventHandler E;
+			public event EventHandler F;
+		}
+		public class B : A
+		{
+			public new event EventHandler E;
+			public new event EventHandler F;
+		}
+		public class C : B
+		{
+			public override event EventHandler E;
+		}
+	}
 }
