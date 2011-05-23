dd02632a4bad9307d01df5bc3bd94fc4d6732f1b
Siegfried Pammer <siegfriedpammer@gmail.com>
2011-05-23 16:57:39 +0200

    add ILSpy.BamlDecompiler project

---
+++ ILSpy.BamlDecompiler/MyClass.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) AlphaSierraPapa for the SharpDevelop Team (for details please see \doc\copyright.txt)
+// This code is distributed under the MS-PL (for details please see \doc\MS-PL.txt)
+
+using System;
+using System.Collections.Generic;
+
+namespace ILSpy.BamlDecompiler
+{
+	/// <summary>
+	/// Description of MyClass.
+	/// </summary>
+	public class MyClass
+	{
+		
+	}
+}
