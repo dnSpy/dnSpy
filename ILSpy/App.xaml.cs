3d0d4ee568f87f4713260a54a348f7c805f53822
Eusebiu Marcu <marcueusebiu@gmail.com>
2011-06-05 09:12:15 +0200

    rename debugger assembly into ILSpy.Debugger.Plugin

--- ILSpy/App.xaml.cs
+++ ILSpy/App.xaml.cs
@@ -60,7 +60,6 @@ namespace ICSharpCode.ILSpy
 			var catalog = new AggregateCatalog();
 			catalog.Catalogs.Add(new AssemblyCatalog(typeof(App).Assembly));
 			catalog.Catalogs.Add(new DirectoryCatalog(".", "*.Plugin.dll"));
-			catalog.Catalogs.Add(new DirectoryCatalog(".", "*.Debugger.dll"));
 			
 			compositionContainer = new CompositionContainer(catalog);
 			
