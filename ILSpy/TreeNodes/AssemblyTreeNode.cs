65f5427ae61729b32edd10080fde301f1f4f21a2
Daniel Grunwald <daniel@danielgrunwald.de>
2011-06-14 23:33:51 +0200

    Fix #162 Opening a file that's not a valid assembly displays exception stack trace

--- ILSpy/TreeNodes/AssemblyTreeNode.cs
+++ ILSpy/TreeNodes/AssemblyTreeNode.cs
@@ -195,7 +195,17 @@ namespace ICSharpCode.ILSpy.TreeNodes
 
 		public override void Decompile(Language language, ITextOutput output, DecompilationOptions options)
 		{
-			assembly.WaitUntilLoaded(); // necessary so that load errors are passed on to the caller
+			try {
+				assembly.WaitUntilLoaded(); // necessary so that load errors are passed on to the caller
+			} catch (AggregateException ex) {
+				language.WriteCommentLine(output, assembly.FileName);
+				if (ex.InnerException is BadImageFormatException) {
+					language.WriteCommentLine(output, "This file does not contain a managed assembly.");
+					return;
+				} else {
+					throw;
+				}
+			}
 			language.DecompileAssembly(assembly, output, options);
 		}
 
