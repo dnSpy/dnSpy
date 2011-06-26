26633dc27aca65d4dce7694d67d09f4ac1eadb3c
Eusebiu Marcu <marcueusebiu@gmail.com>
2011-06-26 13:17:22 +0200

    use LGPL in debugger plugin.

--- Debugger/ILSpy.Debugger/UI/ExecuteProcessWindow.xaml.cs
+++ Debugger/ILSpy.Debugger/UI/ExecuteProcessWindow.xaml.cs
@@ -1,11 +1,6 @@
-﻿/*
- * Created by SharpDevelop.
- * User: klier
- * Date: 05/13/2011
- * Time: 08:44
- * 
- * To change this template use Tools | Options | Coding | Edit Standard Headers.
- */
+﻿// Copyright (c) AlphaSierraPapa for the SharpDevelop Team (for details please see \doc\copyright.txt)
+// This code is distributed under the GNU LGPL (for details please see \doc\license.txt)
+
 using System;
 using System.Collections.Generic;
 using System.IO;
