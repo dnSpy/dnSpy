db5fc018aaa81871b94a06b87fec8f69aaaba8e1
Ed Harvey <edharvey@optusnet.com.au>
2011-11-28 09:49:13 +1100

    Move extension method to more appropriate namespace.

--- ILSpy/ExtensionMethods.cs
+++ ILSpy/ExtensionMethods.cs
@@ -35,55 +35,5 @@ namespace ICSharpCode.ILSpy
 				if (!list.Contains(item))
 					list.Add(item);
 		}
-		
-		/// <summary>
-		/// Sets the value of a dependency property on <paramref name="targetObject"/> using a markup extension.
-		/// </summary>
-		/// <remarks>This method does not support markup extensions like x:Static that depend on
-		/// having a XAML file as context.</remarks>
-		public static void SetValueToExtension(this DependencyObject targetObject, DependencyProperty property, MarkupExtension markupExtension)
-		{
-			// This method was copied from ICSharpCode.Core.Presentation (with permission to switch license to X11)
-			
-			if (targetObject == null)
-				throw new ArgumentNullException("targetObject");
-			if (property == null)
-				throw new ArgumentNullException("property");
-			if (markupExtension == null)
-				throw new ArgumentNullException("markupExtension");
-			
-			var serviceProvider = new SetValueToExtensionServiceProvider(targetObject, property);
-			targetObject.SetValue(property, markupExtension.ProvideValue(serviceProvider));
-		}
-		
-		sealed class SetValueToExtensionServiceProvider : IServiceProvider, IProvideValueTarget
-		{
-			// This class was copied from ICSharpCode.Core.Presentation (with permission to switch license to X11)
-			
-			readonly DependencyObject targetObject;
-			readonly DependencyProperty targetProperty;
-			
-			public SetValueToExtensionServiceProvider(DependencyObject targetObject, DependencyProperty property)
-			{
-				this.targetObject = targetObject;
-				this.targetProperty = property;
-			}
-			
-			public object GetService(Type serviceType)
-			{
-				if (serviceType == typeof(IProvideValueTarget))
-					return this;
-				else
-					return null;
-			}
-			
-			public object TargetObject {
-				get { return targetObject; }
-			}
-			
-			public object TargetProperty {
-				get { return targetProperty; }
-			}
-		}
 	}
 }
