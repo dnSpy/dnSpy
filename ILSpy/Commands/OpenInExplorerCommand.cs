5d4c03af8e17240cf4b24e1ccd35e01ded0a5104
de4dot <de4dot@gmail.com>
2015-03-02 18:12:27 +0100

    Add open in explorer command

---
+++ ILSpy/Commands/OpenInExplorerCommand.cs
@@ -0,0 +1,38 @@
+﻿
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using ICSharpCode.ILSpy.TreeNodes;
+
+namespace ICSharpCode.ILSpy.Commands {
+	[ExportContextMenuEntryAttribute(Header = "_Show in Explorer")]
+	class OpenInExplorerCommand : IContextMenuEntry
+	{
+		public bool IsVisible(TextViewContext context)
+		{
+			return context.SelectedTreeNodes != null &&
+				context.SelectedTreeNodes.Length == 1 &&
+				context.SelectedTreeNodes[0] is AssemblyTreeNode;
+		}
+
+		public bool IsEnabled(TextViewContext context)
+		{
+			return IsVisible(context);
+		}
+
+		public void Execute(TextViewContext context)
+		{
+			// Known problem: explorer can't show files in the .NET 2.0 GAC.
+			var asmNode = (AssemblyTreeNode)context.SelectedTreeNodes[0];
+			var filename = asmNode.LoadedAssembly.FileName;
+			var args = string.Format("/select,{0}", filename);
+			try {
+				Process.Start(new ProcessStartInfo("explorer.exe", args));
+			}
+			catch (IOException) {
+			}
+			catch (Win32Exception) {
+			}
+		}
+	}
+}
