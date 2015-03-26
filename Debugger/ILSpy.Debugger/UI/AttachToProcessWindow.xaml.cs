a3aa851f734c91bc61936a2a07cb2a25d4b9ba69
de4dot <de4dot@gmail.com>
2015-03-26 20:49:06 +0100

    Attach dialog updates
    - Speed up detection (prevent lots of exceptions)
    - Update detection of .NET process (check for clr.dll and mscorwks/svr.dll)

--- Debugger/ILSpy.Debugger/UI/AttachToProcessWindow.xaml.cs
+++ Debugger/ILSpy.Debugger/UI/AttachToProcessWindow.xaml.cs
@@ -7,7 +7,11 @@ using System.ComponentModel;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Threading;
+using Microsoft.Win32.SafeHandles;
 
 using ICSharpCode.ILSpy.Debugger.Models;
 
@@ -34,6 +38,16 @@ namespace ICSharpCode.ILSpy.Debugger.UI
 			}
 		}
 
+		[DllImport("kernel32")]
+		static extern SafeFileHandle OpenProcess(int dwDesiredAccess, bool bInheritHandle, int dwProcessId);
+		const int STANDARD_RIGHTS_REQUIRED = 0x000F0000;
+		const int SYNCHRONIZE = 0x00100000;
+		const int PROCESS_ALL_ACCESS = STANDARD_RIGHTS_REQUIRED | SYNCHRONIZE | 0xFFFF;
+
+		[DllImport("kernel32", SetLastError = true)]
+		[return: MarshalAs(UnmanagedType.Bool)]
+		internal static extern bool IsWow64Process([In] IntPtr hProcess, [Out] out bool Wow64Process);
+
 		void RefreshProcessList()
 		{
 			ObservableCollection<RunningProcess> list = new ObservableCollection<RunningProcess>();
@@ -41,15 +55,23 @@ namespace ICSharpCode.ILSpy.Debugger.UI
 			Process currentProcess = Process.GetCurrentProcess();
 			foreach (Process process in Process.GetProcesses()) {
 				try {
+					// Prevent slow exceptions by filtering out processes we can't access
+					using (var fh = OpenProcess(PROCESS_ALL_ACCESS, false, process.Id)) {
+						if (fh.IsInvalid)
+							continue;
+					}
+					bool isWow64Process;
+					if (IsWow64Process(process.Handle, out isWow64Process)) {
+						if (IntPtr.Size == 4 && !isWow64Process)
+							continue;
+					}
+
 					if (process.HasExited) continue;
 					// Prevent attaching to our own process.
 					if (currentProcess.Id != process.Id) {
 						bool managed = false;
 						try {
-							//TODO: Why isn't WindowsDebugger.IsManaged() used?
-							var modules = process.Modules.Cast<ProcessModule>().Where(
-								m => m.ModuleName.StartsWith("mscor", StringComparison.OrdinalIgnoreCase));
-							
+							var modules = process.Modules.Cast<ProcessModule>().Where(IsNetModule);
 							managed = modules.Count() > 0;
 						} catch { }
 						
@@ -70,6 +92,28 @@ namespace ICSharpCode.ILSpy.Debugger.UI
 			}
 			
 			RunningProcesses.ItemsSource = list;
+			if (list.Count > 0) {
+				RunningProcesses.SelectedIndex = 0;
+				RunningProcesses.Focus();
+				// We must wait a little bit for it to create the ListViewItem
+				Dispatcher.BeginInvoke(DispatcherPriority.Normal, (Action)(() => {
+					var item = RunningProcesses.ItemContainerGenerator.ContainerFromIndex(0) as ListViewItem;
+					if (item != null)
+						item.Focus();
+				}));
+			}
+		}
+
+		static bool IsNetModule(ProcessModule m)
+		{
+			var s = m.ModuleName;
+			if (s.Equals("mscorwks.dll", StringComparison.OrdinalIgnoreCase))
+				return true;
+			if (s.Equals("mscorsvr.dll", StringComparison.OrdinalIgnoreCase))
+				return true;
+			if (s.Equals("clr.dll", StringComparison.OrdinalIgnoreCase))
+				return true;
+			return false;
 		}
 		
 		void Attach()
