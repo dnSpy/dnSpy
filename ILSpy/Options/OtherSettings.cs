52e9f8ded89fabf8084af5998ee5dd2e10cc90de
yck1509 <confuser.net@gmail.com>
2015-07-03 18:26:27 +0800

    Add Windows Explorer integration

--- ILSpy/Options/OtherSettings.cs
+++ ILSpy/Options/OtherSettings.cs
@@ -17,8 +17,12 @@
     along with dnSpy.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+using System;
+using System.IO;
+using System.Security;
 using System.Threading;
 using System.Xml.Linq;
+using Microsoft.Win32;
 
 namespace ICSharpCode.ILSpy.Options
 {
@@ -44,6 +48,14 @@ namespace ICSharpCode.ILSpy.Options
 			}
 		}
 		bool useMemoryMappedIO;
+
+		public bool WindowsExplorerIntegration {
+			get { return HasExplorerIntegration(); }
+			set {
+				SetExplorerIntegration(value);
+				OnPropertyChanged("WindowsExplorerIntegration");
+			}
+		}
 
 		const string SETTINGS_SECTION_NAME = "OtherSettings";
 		internal static OtherSettings Load(ILSpySettings settings)
@@ -87,6 +99,62 @@ namespace ICSharpCode.ILSpy.Options
 
 		public override bool HasError {
 			get { return false; }
+		}
+
+		const string EXPLORER_MENU_TEXT = "Open with dnSpy";
+
+		bool HasExplorerIntegration() {
+			bool hasIntegration = false;
+			try {
+				using (var key = Registry.ClassesRoot.CreateSubKey("exefile\\shell"))
+				using (var ctxMenu = key.OpenSubKey(EXPLORER_MENU_TEXT)) {
+					if (ctxMenu != null)
+						hasIntegration = true;
+				}
+			}
+			catch {
+			}
+			return hasIntegration;
+		}
+
+		void SetExplorerIntegration(bool enabled) {
+			var path = typeof(MainWindow).Assembly.Location;
+			if (!File.Exists(path)) {
+				MainWindow.Instance.ShowMessageBox("Cannot locate dnSpy!");
+				return;
+			}
+			path = string.Format("\"{0}\" \"%1\"", path);
+
+			try {
+				using (var key = Registry.ClassesRoot.CreateSubKey("exefile\\shell")) {
+					if (enabled) {
+						using (var ctxMenu = key.CreateSubKey(EXPLORER_MENU_TEXT))
+						using (var cmdKey = ctxMenu.CreateSubKey("command"))
+							cmdKey.SetValue("", path);
+					}
+					else
+						key.DeleteSubKey(EXPLORER_MENU_TEXT, false);
+				}
+
+				using (var key = Registry.ClassesRoot.CreateSubKey("dllfile\\shell")) {
+					if (enabled) {
+						using (var ctxMenu = key.CreateSubKey(EXPLORER_MENU_TEXT))
+						using (var cmdKey = ctxMenu.CreateSubKey("command"))
+							cmdKey.SetValue("", path);
+					}
+					else
+						key.DeleteSubKey(EXPLORER_MENU_TEXT, false);
+				}
+			}
+			catch (UnauthorizedAccessException) {
+				MainWindow.Instance.ShowMessageBox("Cannot obtain access to registry!");
+			}
+			catch (SecurityException) {
+				MainWindow.Instance.ShowMessageBox("Cannot obtain access to registry!");
+			}
+			catch (Exception ex) {
+				MainWindow.Instance.ShowMessageBox("Cannot add context menu item!" + Environment.NewLine + ex.ToString());
+			}
 		}
 	}
 }
