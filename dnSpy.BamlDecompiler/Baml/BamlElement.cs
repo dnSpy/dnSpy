185646b3bfa347b6549cd636345f961e913859b4
yck1509 <confuser.net@gmail.com>
2015-07-04 16:50:26 +0800

    Import BAML R/W library

---
+++ dnSpy.BamlDecompiler/Baml/BamlElement.cs
@@ -0,0 +1,142 @@
+﻿/*
+	Copyright (c) 2015 Ki
+
+	Permission is hereby granted, free of charge, to any person obtaining a copy
+	of this software and associated documentation files (the "Software"), to deal
+	in the Software without restriction, including without limitation the rights
+	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+	copies of the Software, and to permit persons to whom the Software is
+	furnished to do so, subject to the following conditions:
+
+	The above copyright notice and this permission notice shall be included in
+	all copies or substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+	THE SOFTWARE.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace dnSpy.BamlDecompiler.Baml {
+	internal class BamlElement {
+		public BamlElement Parent { get; private set; }
+		public BamlRecord Header { get; private set; }
+		public IList<BamlRecord> Body { get; private set; }
+		public IList<BamlElement> Children { get; private set; }
+		public BamlRecord Footer { get; private set; }
+
+		static bool IsHeader(BamlRecord rec) {
+			switch (rec.Type) {
+				case BamlRecordType.ConstructorParametersStart:
+				case BamlRecordType.DocumentStart:
+				case BamlRecordType.ElementStart:
+				case BamlRecordType.KeyElementStart:
+				case BamlRecordType.NamedElementStart:
+				case BamlRecordType.PropertyArrayStart:
+				case BamlRecordType.PropertyComplexStart:
+				case BamlRecordType.PropertyDictionaryStart:
+				case BamlRecordType.PropertyListStart:
+				case BamlRecordType.StaticResourceStart:
+					return true;
+			}
+			return false;
+		}
+
+		static bool IsFooter(BamlRecord rec) {
+			switch (rec.Type) {
+				case BamlRecordType.ConstructorParametersEnd:
+				case BamlRecordType.DocumentEnd:
+				case BamlRecordType.ElementEnd:
+				case BamlRecordType.KeyElementEnd:
+				case BamlRecordType.PropertyArrayEnd:
+				case BamlRecordType.PropertyComplexEnd:
+				case BamlRecordType.PropertyDictionaryEnd:
+				case BamlRecordType.PropertyListEnd:
+				case BamlRecordType.StaticResourceEnd:
+					return true;
+			}
+			return false;
+		}
+
+		static bool IsMatch(BamlRecord header, BamlRecord footer) {
+			switch (header.Type) {
+				case BamlRecordType.ConstructorParametersStart:
+					return footer.Type == BamlRecordType.ConstructorParametersEnd;
+
+				case BamlRecordType.DocumentStart:
+					return footer.Type == BamlRecordType.DocumentEnd;
+
+				case BamlRecordType.KeyElementStart:
+					return footer.Type == BamlRecordType.KeyElementEnd;
+
+				case BamlRecordType.PropertyArrayStart:
+					return footer.Type == BamlRecordType.PropertyArrayEnd;
+
+				case BamlRecordType.PropertyComplexStart:
+					return footer.Type == BamlRecordType.PropertyComplexEnd;
+
+				case BamlRecordType.PropertyDictionaryStart:
+					return footer.Type == BamlRecordType.PropertyDictionaryEnd;
+
+				case BamlRecordType.PropertyListStart:
+					return footer.Type == BamlRecordType.PropertyListEnd;
+
+				case BamlRecordType.StaticResourceStart:
+					return footer.Type == BamlRecordType.StaticResourceEnd;
+
+				case BamlRecordType.ElementStart:
+				case BamlRecordType.NamedElementStart:
+					return footer.Type == BamlRecordType.ElementEnd;
+			}
+			return false;
+		}
+
+		public static BamlElement Read(BamlDocument document) {
+			Debug.Assert(document.Count > 0 && document[0].Type == BamlRecordType.DocumentStart);
+
+			BamlElement current = null;
+			var stack = new Stack<BamlElement>();
+
+			for (int i = 0; i < document.Count; i++) {
+				if (IsHeader(document[i])) {
+					BamlElement prev = current;
+
+					current = new BamlElement();
+					current.Header = document[i];
+					current.Body = new List<BamlRecord>();
+					current.Children = new List<BamlElement>();
+
+					if (prev != null) {
+						prev.Children.Add(current);
+						current.Parent = prev;
+						stack.Push(prev);
+					}
+				}
+				else if (IsFooter(document[i])) {
+					if (current == null)
+						throw new Exception("Unexpected footer.");
+
+					while (!IsMatch(current.Header, document[i])) {
+						// End record can be omited (sometimes).
+						if (stack.Count > 0)
+							current = stack.Pop();
+					}
+					current.Footer = document[i];
+					if (stack.Count > 0)
+						current = stack.Pop();
+				}
+				else
+					current.Body.Add(document[i]);
+			}
+			Debug.Assert(stack.Count == 0);
+			return current;
+		}
+	}
+}
---
+++ dnSpy.BamlDecompiler/Baml/BamlReader.cs
@@ -0,0 +1,239 @@
+﻿/*
+	Copyright (c) 2015 Ki
+
+	Permission is hereby granted, free of charge, to any person obtaining a copy
+	of this software and associated documentation files (the "Software"), to deal
+	in the Software without restriction, including without limitation the rights
+	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+	copies of the Software, and to permit persons to whom the Software is
+	furnished to do so, subject to the following conditions:
+
+	The above copyright notice and this permission notice shall be included in
+	all copies or substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+	THE SOFTWARE.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace dnSpy.BamlDecompiler.Baml {
+	internal class BamlBinaryReader : BinaryReader {
+		public BamlBinaryReader(Stream stream)
+			: base(stream) {
+		}
+
+		public int ReadEncodedInt() {
+			return Read7BitEncodedInt();
+		}
+	}
+
+	internal class BamlReader {
+		public static BamlDocument ReadDocument(Stream str) {
+			var ret = new BamlDocument();
+			var reader = new BamlBinaryReader(str);
+			{
+				var rdr = new BinaryReader(str, Encoding.Unicode);
+				uint len = rdr.ReadUInt32();
+				ret.Signature = new string(rdr.ReadChars((int)(len >> 1)));
+				rdr.ReadBytes((int)(((len + 3) & ~3) - len));
+			}
+			if (ret.Signature != "MSBAML") throw new NotSupportedException();
+			ret.ReaderVersion = new BamlDocument.BamlVersion { Major = reader.ReadUInt16(), Minor = reader.ReadUInt16() };
+			ret.UpdaterVersion = new BamlDocument.BamlVersion { Major = reader.ReadUInt16(), Minor = reader.ReadUInt16() };
+			ret.WriterVersion = new BamlDocument.BamlVersion { Major = reader.ReadUInt16(), Minor = reader.ReadUInt16() };
+			if (ret.ReaderVersion.Major != 0 || ret.ReaderVersion.Minor != 0x60 ||
+			    ret.UpdaterVersion.Major != 0 || ret.UpdaterVersion.Minor != 0x60 ||
+			    ret.WriterVersion.Major != 0 || ret.WriterVersion.Minor != 0x60)
+				throw new NotSupportedException();
+
+			var recs = new Dictionary<long, BamlRecord>();
+			while (str.Position < str.Length) {
+				long pos = str.Position;
+				var type = (BamlRecordType)reader.ReadByte();
+				BamlRecord rec = null;
+				switch (type) {
+					case BamlRecordType.AssemblyInfo:
+						rec = new AssemblyInfoRecord();
+						break;
+					case BamlRecordType.AttributeInfo:
+						rec = new AttributeInfoRecord();
+						break;
+					case BamlRecordType.ConstructorParametersStart:
+						rec = new ConstructorParametersStartRecord();
+						break;
+					case BamlRecordType.ConstructorParametersEnd:
+						rec = new ConstructorParametersEndRecord();
+						break;
+					case BamlRecordType.ConstructorParameterType:
+						rec = new ConstructorParameterTypeRecord();
+						break;
+					case BamlRecordType.ConnectionId:
+						rec = new ConnectionIdRecord();
+						break;
+					case BamlRecordType.ContentProperty:
+						rec = new ContentPropertyRecord();
+						break;
+					case BamlRecordType.DefAttribute:
+						rec = new DefAttributeRecord();
+						break;
+					case BamlRecordType.DefAttributeKeyString:
+						rec = new DefAttributeKeyStringRecord();
+						break;
+					case BamlRecordType.DefAttributeKeyType:
+						rec = new DefAttributeKeyTypeRecord();
+						break;
+					case BamlRecordType.DeferableContentStart:
+						rec = new DeferableContentStartRecord();
+						break;
+					case BamlRecordType.DocumentEnd:
+						rec = new DocumentEndRecord();
+						break;
+					case BamlRecordType.DocumentStart:
+						rec = new DocumentStartRecord();
+						break;
+					case BamlRecordType.ElementEnd:
+						rec = new ElementEndRecord();
+						break;
+					case BamlRecordType.ElementStart:
+						rec = new ElementStartRecord();
+						break;
+					case BamlRecordType.KeyElementEnd:
+						rec = new KeyElementEndRecord();
+						break;
+					case BamlRecordType.KeyElementStart:
+						rec = new KeyElementStartRecord();
+						break;
+					case BamlRecordType.LineNumberAndPosition:
+						rec = new LineNumberAndPositionRecord();
+						break;
+					case BamlRecordType.LinePosition:
+						rec = new LinePositionRecord();
+						break;
+					case BamlRecordType.LiteralContent:
+						rec = new LiteralContentRecord();
+						break;
+					case BamlRecordType.NamedElementStart:
+						rec = new NamedElementStartRecord();
+						break;
+					case BamlRecordType.OptimizedStaticResource:
+						rec = new OptimizedStaticResourceRecord();
+						break;
+					case BamlRecordType.PIMapping:
+						rec = new PIMappingRecord();
+						break;
+					case BamlRecordType.PresentationOptionsAttribute:
+						rec = new PresentationOptionsAttributeRecord();
+						break;
+					case BamlRecordType.Property:
+						rec = new PropertyRecord();
+						break;
+					case BamlRecordType.PropertyArrayEnd:
+						rec = new PropertyArrayEndRecord();
+						break;
+					case BamlRecordType.PropertyArrayStart:
+						rec = new PropertyArrayStartRecord();
+						break;
+					case BamlRecordType.PropertyComplexEnd:
+						rec = new PropertyComplexEndRecord();
+						break;
+					case BamlRecordType.PropertyComplexStart:
+						rec = new PropertyComplexStartRecord();
+						break;
+					case BamlRecordType.PropertyCustom:
+						rec = new PropertyCustomRecord();
+						break;
+					case BamlRecordType.PropertyDictionaryEnd:
+						rec = new PropertyDictionaryEndRecord();
+						break;
+					case BamlRecordType.PropertyDictionaryStart:
+						rec = new PropertyDictionaryStartRecord();
+						break;
+					case BamlRecordType.PropertyListEnd:
+						rec = new PropertyListEndRecord();
+						break;
+					case BamlRecordType.PropertyListStart:
+						rec = new PropertyListStartRecord();
+						break;
+					case BamlRecordType.PropertyStringReference:
+						rec = new PropertyStringReferenceRecord();
+						break;
+					case BamlRecordType.PropertyTypeReference:
+						rec = new PropertyTypeReferenceRecord();
+						break;
+					case BamlRecordType.PropertyWithConverter:
+						rec = new PropertyWithConverterRecord();
+						break;
+					case BamlRecordType.PropertyWithExtension:
+						rec = new PropertyWithExtensionRecord();
+						break;
+					case BamlRecordType.PropertyWithStaticResourceId:
+						rec = new PropertyWithStaticResourceIdRecord();
+						break;
+					case BamlRecordType.RoutedEvent:
+						rec = new RoutedEventRecord();
+						break;
+					case BamlRecordType.StaticResourceEnd:
+						rec = new StaticResourceEndRecord();
+						break;
+					case BamlRecordType.StaticResourceId:
+						rec = new StaticResourceIdRecord();
+						break;
+					case BamlRecordType.StaticResourceStart:
+						rec = new StaticResourceStartRecord();
+						break;
+					case BamlRecordType.StringInfo:
+						rec = new StringInfoRecord();
+						break;
+					case BamlRecordType.Text:
+						rec = new TextRecord();
+						break;
+					case BamlRecordType.TextWithConverter:
+						rec = new TextWithConverterRecord();
+						break;
+					case BamlRecordType.TextWithId:
+						rec = new TextWithIdRecord();
+						break;
+					case BamlRecordType.TypeInfo:
+						rec = new TypeInfoRecord();
+						break;
+					case BamlRecordType.TypeSerializerInfo:
+						rec = new TypeSerializerInfoRecord();
+						break;
+					case BamlRecordType.XmlnsProperty:
+						rec = new XmlnsPropertyRecord();
+						break;
+					case BamlRecordType.XmlAttribute:
+					case BamlRecordType.ProcessingInstruction:
+					case BamlRecordType.LastRecordType:
+					case BamlRecordType.EndAttributes:
+					case BamlRecordType.DefTag:
+					case BamlRecordType.ClrEvent:
+					case BamlRecordType.Comment:
+					default:
+						throw new NotSupportedException();
+				}
+				rec.Position = pos;
+
+				rec.Read(reader);
+				ret.Add(rec);
+				recs.Add(pos, rec);
+			}
+			for (int i = 0; i < ret.Count; i++) {
+				var defer = ret[i] as IBamlDeferRecord;
+				if (defer != null)
+					defer.ReadDefer(ret, i, _ => recs[_]);
+			}
+
+			return ret;
+		}
+	}
+}
