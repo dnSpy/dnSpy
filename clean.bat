2e990a57bf691119f37df8ca486dc5ee500e3f01
Ken Wilcox <wilcoxk@medamerica.com>
2015-10-13 08:55:20 -0600

    Renamed ILSpy to dnSpy

--- clean.bat
+++ clean.bat
@@ -1,4 +1,4 @@
-%windir%\microsoft.net\framework\v4.0.30319\msbuild /m ILSpy.sln /t:clean "/p:Platform=Any CPU" /p:Configuration=Debug
+%windir%\microsoft.net\framework\v4.0.30319\msbuild /m dnSpy.sln /t:clean "/p:Platform=Any CPU" /p:Configuration=Debug
 @IF %ERRORLEVEL% NEQ 0 PAUSE
-%windir%\microsoft.net\framework\v4.0.30319\msbuild /m ILSpy.sln /t:clean "/p:Platform=Any CPU" /p:Configuration=Release
+%windir%\microsoft.net\framework\v4.0.30319\msbuild /m dnSpy.sln /t:clean "/p:Platform=Any CPU" /p:Configuration=Release
 @IF %ERRORLEVEL% NEQ 0 PAUSE
