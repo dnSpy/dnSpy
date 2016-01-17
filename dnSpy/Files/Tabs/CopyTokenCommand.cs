ed83b16a481a8a81e9c4fb2492edcbc4f99839df
de4dot <de4dot@gmail.com>
2016-01-17 20:24:25 +0100

    Update copy-md-token cmd to also copy tokens from TokenReferences

--- dnSpy/Files/Tabs/CopyTokenCommand.cs
+++ dnSpy/Files/Tabs/CopyTokenCommand.cs
@@ -23,14 +23,14 @@ using dnlib.DotNet;
 using dnSpy.Contracts.Files.Tabs.TextEditor;
 using dnSpy.Contracts.Menus;
 using dnSpy.Contracts.TreeView;
+using dnSpy.Decompiler.Shared;
 using dnSpy.Shared.Menus;
 
 namespace dnSpy.Files.Tabs {
 	static class CopyTokenCommand {
-		static void ExecuteInternal(IMDTokenProvider member) {
-			if (member == null)
-				return;
-			Clipboard.SetText(string.Format("0x{0:X8}", member.MDToken.Raw));
+		static void ExecuteInternal(uint? token) {
+			if (token != null)
+				Clipboard.SetText(string.Format("0x{0:X8}", token.Value));
 		}
 
 		[ExportMenuItem(Header = "res:CopyMDTokenCommand", Group = MenuConstants.GROUP_CTX_CODE_TOKENS, Order = 50)]
@@ -43,17 +43,19 @@ namespace dnSpy.Files.Tabs {
 				ExecuteInternal(GetReference(context));
 			}
 
-			static IMDTokenProvider GetReference(IMenuItemContext context) {
+			static uint? GetReference(IMenuItemContext context) {
 				return GetReference(context, MenuConstants.GUIDOBJ_TEXTEDITORCONTROL_GUID);
 			}
 
-			internal static IMDTokenProvider GetReference(IMenuItemContext context, string guid) {
+			internal static uint? GetReference(IMenuItemContext context, string guid) {
 				if (context.CreatorObject.Guid != new Guid(guid))
 					return null;
 				var @ref = context.Find<CodeReference>();
-				if (@ref == null)
-					return null;
-				return @ref.Reference as IMDTokenProvider;
+				if (@ref != null && @ref.Reference is IMDTokenProvider)
+					return ((IMDTokenProvider)@ref.Reference).MDToken.Raw;
+				if (@ref != null && @ref.Reference is TokenReference)
+					return ((TokenReference)@ref.Reference).Token;
+				return null;
 			}
 		}
 
@@ -67,7 +69,7 @@ namespace dnSpy.Files.Tabs {
 				ExecuteInternal(GetReference(context));
 			}
 
-			static IMDTokenProvider GetReference(IMenuItemContext context) {
+			static uint? GetReference(IMenuItemContext context) {
 				return CodeCommand.GetReference(context, MenuConstants.GUIDOBJ_SEARCH_GUID);
 			}
 		}
@@ -82,18 +84,18 @@ namespace dnSpy.Files.Tabs {
 				ExecuteInternal(GetReference(context));
 			}
 
-			static IMDTokenProvider GetReference(IMenuItemContext context) {
+			static uint? GetReference(IMenuItemContext context) {
 				return GetReference(context, MenuConstants.GUIDOBJ_FILES_TREEVIEW_GUID);
 			}
 
-			internal static IMDTokenProvider GetReference(IMenuItemContext context, string guid) {
+			internal static uint? GetReference(IMenuItemContext context, string guid) {
 				if (context.CreatorObject.Guid != new Guid(guid))
 					return null;
 				var nodes = context.Find<ITreeNodeData[]>();
 				if (nodes == null || nodes.Length == 0)
 					return null;
 				var node = nodes[0] as IMDTokenNode;
-				return node == null ? null : node.Reference;
+				return node == null ? (uint?)null : node.Reference == null ? (uint?)null : node.Reference.MDToken.Raw;
 			}
 		}
 
@@ -107,7 +109,7 @@ namespace dnSpy.Files.Tabs {
 				ExecuteInternal(GetReference(context));
 			}
 
-			static IMDTokenProvider GetReference(IMenuItemContext context) {
+			static uint? GetReference(IMenuItemContext context) {
 				return FilesCommand.GetReference(context, MenuConstants.GUIDOBJ_ANALYZER_TREEVIEW_GUID);
 			}
 		}
