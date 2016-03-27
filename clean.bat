98e77e4cc9768009a6c5075311105ea963777a42
de4dot <de4dot@gmail.com>
2016-03-27 18:51:25 +0200

    Switch to VS2015 and C# 6. Fixes #98

--- clean.bat
+++
@@ -1,4 +0,0 @@
-%windir%\microsoft.net\framework\v4.0.30319\msbuild /m dnSpy.sln /t:clean "/p:Platform=Any CPU" /p:Configuration=Debug
-@IF %ERRORLEVEL% NEQ 0 PAUSE
-%windir%\microsoft.net\framework\v4.0.30319\msbuild /m dnSpy.sln /t:clean "/p:Platform=Any CPU" /p:Configuration=Release
-@IF %ERRORLEVEL% NEQ 0 PAUSE
--- debugbuild.bat
+++
@@ -1,6 +0,0 @@
-%windir%\microsoft.net\framework\v4.0.30319\msbuild /m dnSpy.sln /p:Configuration=Debug "/p:Platform=Any CPU"
-@IF %ERRORLEVEL% NEQ 0 GOTO err
-@exit /B 0
-:err
-@PAUSE
-@exit /B 1
