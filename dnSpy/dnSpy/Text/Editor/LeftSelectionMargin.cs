05eae429200ee35cb8ce092779f49561d1aa5af8
de4dot <de4dot@gmail.com>
2016-07-13 20:35:15 +0200

    Add mouse events to left selection margin

--- dnSpy/dnSpy/Text/Editor/LeftSelectionMargin.cs
+++ dnSpy/dnSpy/Text/Editor/LeftSelectionMargin.cs
@@ -17,9 +17,14 @@
     along with dnSpy.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+using System;
 using System.ComponentModel.Composition;
+using System.Windows.Input;
+using System.Windows.Media;
 using dnSpy.Contracts.Text;
+using Microsoft.VisualStudio.Text;
 using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.Text.Operations;
 using Microsoft.VisualStudio.Utilities;
 
 namespace dnSpy.Text.Editor {
@@ -30,21 +35,92 @@ namespace dnSpy.Text.Editor {
 	[TextViewRole(PredefinedTextViewRoles.Interactive)]
 	sealed class LeftSelectionMarginProvider : IWpfTextViewMarginProvider {
 		readonly IWpfTextViewMarginProviderCollectionCreator wpfTextViewMarginProviderCollectionCreator;
+		readonly IEditorOperationsFactoryService editorOperationsFactoryService;
 
 		[ImportingConstructor]
-		LeftSelectionMarginProvider(IWpfTextViewMarginProviderCollectionCreator wpfTextViewMarginProviderCollectionCreator) {
+		LeftSelectionMarginProvider(IWpfTextViewMarginProviderCollectionCreator wpfTextViewMarginProviderCollectionCreator, IEditorOperationsFactoryService editorOperationsFactoryService) {
 			this.wpfTextViewMarginProviderCollectionCreator = wpfTextViewMarginProviderCollectionCreator;
+			this.editorOperationsFactoryService = editorOperationsFactoryService;
 		}
 
 		public IWpfTextViewMargin CreateMargin(IWpfTextViewHost wpfTextViewHost, IWpfTextViewMargin marginContainer) =>
-			new LeftSelectionMargin(wpfTextViewMarginProviderCollectionCreator, wpfTextViewHost);
+			new LeftSelectionMargin(wpfTextViewMarginProviderCollectionCreator, wpfTextViewHost, editorOperationsFactoryService.GetEditorOperations(wpfTextViewHost.TextView));
 	}
 
 	sealed class LeftSelectionMargin : WpfTextViewContainerMargin {
-		public LeftSelectionMargin(IWpfTextViewMarginProviderCollectionCreator wpfTextViewMarginProviderCollectionCreator, IWpfTextViewHost wpfTextViewHost)
+		readonly IWpfTextViewHost wpfTextViewHost;
+		readonly IEditorOperations editorOperations;
+
+		public LeftSelectionMargin(IWpfTextViewMarginProviderCollectionCreator wpfTextViewMarginProviderCollectionCreator, IWpfTextViewHost wpfTextViewHost, IEditorOperations editorOperations)
 			: base(wpfTextViewMarginProviderCollectionCreator, wpfTextViewHost, PredefinedMarginNames.LeftSelection, false) {
+			if (editorOperations == null)
+				throw new ArgumentNullException(nameof(editorOperations));
+			this.Cursor = Cursors.Arrow;//TODO: Use an arrow pointing to the right
+			this.wpfTextViewHost = wpfTextViewHost;
+			this.editorOperations = editorOperations;
+			wpfTextViewHost.TextView.ZoomLevelChanged += TextView_ZoomLevelChanged;
+			// Make sure that the user can click anywhere in this margin so we'll get mouse events
+			this.Background = Brushes.Transparent;
 		}
 
-		//TODO: mouse selection
+		void TextView_ZoomLevelChanged(object sender, ZoomLevelChangedEventArgs e) => LayoutTransform = e.ZoomTransform;
+		protected override void DisposeInternal() => wpfTextViewHost.TextView.ZoomLevelChanged -= TextView_ZoomLevelChanged;
+
+		protected override void OnMouseLeftButtonDown(MouseButtonEventArgs e) {
+			if (CaptureMouse()) {
+				var line = MouseLocation.Create(wpfTextViewHost.TextView, e).TextViewLine;
+				editorOperations.SelectLine(line, (Keyboard.Modifiers & ModifierKeys.Shift) != 0);
+				mouseCaptured = true;
+				e.Handled = true;
+				return;
+			}
+
+			base.OnMouseLeftButtonDown(e);
+		}
+		bool mouseCaptured;
+
+		protected override void OnMouseMove(MouseEventArgs e) {
+			if (mouseCaptured) {
+				var mouseLoc = MouseLocation.Create(wpfTextViewHost.TextView, e);
+				var line = mouseLoc.TextViewLine;
+				editorOperations.SelectLine(line, true);
+				// Needed or the scrolling will stop
+				if (mouseLoc.Point.Y <= wpfTextViewHost.TextView.ViewportTop)
+					editorOperations.ScrollUpAndMoveCaretIfNecessary();
+				else if (mouseLoc.Point.Y >= wpfTextViewHost.TextView.ViewportBottom)
+					editorOperations.ScrollDownAndMoveCaretIfNecessary();
+				e.Handled = true;
+				return;
+			}
+
+			base.OnMouseMove(e);
+		}
+
+		void Select(SnapshotPoint a, SnapshotPoint b) {
+			// In case there were any edits, eg. user pressed DEL
+			a = a.TranslateTo(wpfTextViewHost.TextView.TextSnapshot, PointTrackingMode.Negative);
+			b = b.TranslateTo(wpfTextViewHost.TextView.TextSnapshot, PointTrackingMode.Negative);
+			wpfTextViewHost.TextView.Selection.Mode = TextSelectionMode.Stream;
+			var line1 = wpfTextViewHost.TextView.GetTextViewLineContainingBufferPosition(a);
+			var line2 = wpfTextViewHost.TextView.GetTextViewLineContainingBufferPosition(b);
+			wpfTextViewHost.TextView.Selection.Mode = TextSelectionMode.Stream;
+			if (line1.Start <= line2.Start)
+				wpfTextViewHost.TextView.Selection.Select(new VirtualSnapshotPoint(line1.Start), new VirtualSnapshotPoint(line2.EndIncludingLineBreak));
+			else
+				wpfTextViewHost.TextView.Selection.Select(new VirtualSnapshotPoint(line1.EndIncludingLineBreak), new VirtualSnapshotPoint(line2.Start));
+			wpfTextViewHost.TextView.Caret.MoveTo(wpfTextViewHost.TextView.Selection.ActivePoint);
+			wpfTextViewHost.TextView.Caret.EnsureVisible();
+		}
+
+		protected override void OnMouseLeftButtonUp(MouseButtonEventArgs e) {
+			if (mouseCaptured) {
+				mouseCaptured = false;
+				ReleaseMouseCapture();
+				e.Handled = true;
+				return;
+			}
+
+			base.OnMouseLeftButtonUp(e);
+		}
 	}
 }
