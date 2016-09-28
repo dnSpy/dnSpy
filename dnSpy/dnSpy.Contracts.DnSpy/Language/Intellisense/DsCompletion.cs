6eb76b34563fb21fc75d39c6a35cf7ad82f4658c
de4dot <de4dot@gmail.com>
2016-09-28 23:56:08 +0200

    Make sure interop asm is linked, not referenced

--- dnSpy/dnSpy.Contracts.DnSpy/Language/Intellisense/DsCompletion.cs
+++ dnSpy/dnSpy.Contracts.DnSpy/Language/Intellisense/DsCompletion.cs
@@ -56,13 +56,13 @@ namespace dnSpy.Contracts.Language.Intellisense {
 		/// <param name="iconAutomationText">Icon automation text or null</param>
 		/// <param name="attributeIcons">Attribute icons shown on the right side</param>
 		/// <param name="suffix">Text shown after the normal completion text</param>
-		public DsCompletion(string displayText, string filterText = null, string insertionText = null, string description = null, ImageMoniker? iconMoniker = null, string iconAutomationText = null, IEnumerable<CompletionIcon2> attributeIcons = null, string suffix = null)
+		public DsCompletion(string displayText, string filterText = null, string insertionText = null, string description = null, ImageMoniker iconMoniker = default(ImageMoniker), string iconAutomationText = null, IEnumerable<CompletionIcon2> attributeIcons = null, string suffix = null)
 			: base(displayText, insertionText, description, default(ImageMoniker), iconAutomationText, attributeIcons, suffix) {
 			if (displayText == null)
 				throw new ArgumentNullException(nameof(displayText));
 			FilterText = filterText ?? displayText;
 			InsertionText = insertionText ?? displayText;
-			this.iconMoniker = iconMoniker;
+			this.iconMoniker = iconMoniker.Id == 0 && iconMoniker.Guid == Guid.Empty ? (ImageMoniker?)null : iconMoniker;
 		}
 
 		/// <summary>
