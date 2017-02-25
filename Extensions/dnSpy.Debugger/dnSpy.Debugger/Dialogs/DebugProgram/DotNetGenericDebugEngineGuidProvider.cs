25ca98ee25aebfdd878900e4777f8d33b4ee4022
de4dot <de4dot@gmail.com>
2017-02-25 18:39:42 +0100

    Fix comparison

--- Extensions/dnSpy.Debugger/dnSpy.Debugger/Dialogs/DebugProgram/DotNetGenericDebugEngineGuidProvider.cs
+++ Extensions/dnSpy.Debugger/dnSpy.Debugger/Dialogs/DebugProgram/DotNetGenericDebugEngineGuidProvider.cs
@@ -33,7 +33,7 @@ namespace dnSpy.Debugger.Dialogs.DebugProgram {
 				using (var peImage = new PEImage(filename)) {
 					if ((peImage.ImageNTHeaders.FileHeader.Characteristics & Characteristics.Dll) != 0)
 						return null;
-					if (peImage.ImageNTHeaders.OptionalHeader.DataDirectories.Length < 14)
+					if (peImage.ImageNTHeaders.OptionalHeader.DataDirectories.Length <= 14)
 						return null;
 					var dd = peImage.ImageNTHeaders.OptionalHeader.DataDirectories[14];
 					if (dd.VirtualAddress == 0 || dd.Size < 0x48)
