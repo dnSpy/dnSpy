732720be3030cf0aefba3ec14bec6c55d8f4f287
de4dot <de4dot@gmail.com>
2017-03-07 22:00:54 +0100

    Add threads window commands

---
+++ Extensions/dnSpy.Debugger/dnSpy.Debugger/ToolWindows/Threads/Commands.cs
@@ -0,0 +1,155 @@
+﻿/*
+    Copyright (C) 2014-2017 de4dot@gmail.com
+
+    This file is part of dnSpy
+
+    dnSpy is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    dnSpy is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with dnSpy.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+using System;
+using System.ComponentModel.Composition;
+using System.Windows.Controls;
+using System.Windows.Input;
+using dnSpy.Contracts.Controls;
+using dnSpy.Contracts.Extension;
+using dnSpy.Contracts.Images;
+using dnSpy.Contracts.Menus;
+using dnSpy.Contracts.MVVM;
+
+namespace dnSpy.Debugger.ToolWindows.Threads {
+	[ExportAutoLoaded]
+	sealed class ThreadsCommandsLoader : IAutoLoaded {
+		[ImportingConstructor]
+		ThreadsCommandsLoader(IWpfCommandService wpfCommandService, Lazy<IThreadsContent> threadsContent) {
+			var cmds = wpfCommandService.GetCommands(ControlConstants.GUID_DEBUGGER_THREADS_LISTVIEW);
+			cmds.Add(new RelayCommand(a => threadsContent.Value.Operations.Copy(), a => threadsContent.Value.Operations.CanCopy), ModifierKeys.Control, Key.C);
+			cmds.Add(new RelayCommand(a => threadsContent.Value.Operations.SwitchToThread(newTab: false), a => threadsContent.Value.Operations.CanSwitchToThread), ModifierKeys.None, Key.Enter);
+			cmds.Add(new RelayCommand(a => threadsContent.Value.Operations.SwitchToThread(newTab: true), a => threadsContent.Value.Operations.CanSwitchToThread), ModifierKeys.Control, Key.Enter);
+			cmds.Add(new RelayCommand(a => threadsContent.Value.Operations.SwitchToThread(newTab: true), a => threadsContent.Value.Operations.CanSwitchToThread), ModifierKeys.Shift, Key.Enter);
+		}
+	}
+
+	sealed class ThreadsCtxMenuContext {
+		public ThreadsOperations Operations { get; }
+		public ThreadsCtxMenuContext(ThreadsOperations operations) => Operations = operations;
+	}
+
+	abstract class ThreadsCtxMenuCommand : MenuItemBase<ThreadsCtxMenuContext> {
+		protected sealed override object CachedContextKey => ContextKey;
+		static readonly object ContextKey = new object();
+
+		protected readonly Lazy<IThreadsContent> threadsContent;
+
+		protected ThreadsCtxMenuCommand(Lazy<IThreadsContent> threadsContent) => this.threadsContent = threadsContent;
+
+		protected sealed override ThreadsCtxMenuContext CreateContext(IMenuItemContext context) {
+			if (!(context.CreatorObject.Object is ListView))
+				return null;
+			if (context.CreatorObject.Object != threadsContent.Value.ListView)
+				return null;
+			return Create();
+		}
+
+		ThreadsCtxMenuContext Create() => new ThreadsCtxMenuContext(threadsContent.Value.Operations);
+	}
+
+	[ExportMenuItem(Header = "res:CopyCommand", Icon = DsImagesAttribute.Copy, InputGestureText = "res:ShortCutKeyCtrlC", Group = MenuConstants.GROUP_CTX_DBG_THREADS_COPY, Order = 0)]
+	sealed class CopyThreadsCtxMenuCommand : ThreadsCtxMenuCommand {
+		[ImportingConstructor]
+		CopyThreadsCtxMenuCommand(Lazy<IThreadsContent> threadsContent)
+			: base(threadsContent) {
+		}
+
+		public override void Execute(ThreadsCtxMenuContext context) => context.Operations.Copy();
+		public override bool IsEnabled(ThreadsCtxMenuContext context) => context.Operations.CanCopy;
+	}
+
+	[ExportMenuItem(Header = "res:SelectAllCommand", Icon = DsImagesAttribute.Select, InputGestureText = "res:ShortCutKeyCtrlA", Group = MenuConstants.GROUP_CTX_DBG_THREADS_COPY, Order = 10)]
+	sealed class SelectAllThreadsCtxMenuCommand : ThreadsCtxMenuCommand {
+		[ImportingConstructor]
+		SelectAllThreadsCtxMenuCommand(Lazy<IThreadsContent> threadsContent)
+			: base(threadsContent) {
+		}
+
+		public override void Execute(ThreadsCtxMenuContext context) => context.Operations.SelectAll();
+		public override bool IsEnabled(ThreadsCtxMenuContext context) => context.Operations.CanSelectAll;
+	}
+
+	[ExportMenuItem(Header = "res:HexDisplayCommand", Group = MenuConstants.GROUP_CTX_DBG_THREADS_HEXOPTS, Order = 0)]
+	sealed class UseHexadecimalThreadsCtxMenuCommand : ThreadsCtxMenuCommand {
+		[ImportingConstructor]
+		UseHexadecimalThreadsCtxMenuCommand(Lazy<IThreadsContent> threadesContent)
+			: base(threadesContent) {
+		}
+
+		public override void Execute(ThreadsCtxMenuContext context) => context.Operations.ToggleUseHexadecimal();
+		public override bool IsEnabled(ThreadsCtxMenuContext context) => context.Operations.CanToggleUseHexadecimal;
+		public override bool IsChecked(ThreadsCtxMenuContext context) => context.Operations.UseHexadecimal;
+	}
+
+	[Export, ExportMenuItem(Header = "res:SwitchToThreadCommand", InputGestureText = "res:ShortCutKeyEnter", Group = MenuConstants.GROUP_CTX_DBG_THREADS_CMDS, Order = 0)]
+	sealed class SwitchToThreadThreadsCtxMenuCommand : ThreadsCtxMenuCommand {
+		[ImportingConstructor]
+		SwitchToThreadThreadsCtxMenuCommand(Lazy<IThreadsContent> threadesContent)
+			: base(threadesContent) {
+		}
+
+		public override void Execute(ThreadsCtxMenuContext context) => context.Operations.SwitchToThread(newTab: false);
+		public override bool IsEnabled(ThreadsCtxMenuContext context) => context.Operations.CanSwitchToThread;
+	}
+
+	[ExportMenuItem(Header = "res:SwitchToThreadNewTabCommand", InputGestureText = "res:ShortCutKeyCtrlEnter", Group = MenuConstants.GROUP_CTX_DBG_THREADS_CMDS, Order = 10)]
+	sealed class SwitchToThreadNewTabThreadsCtxMenuCommand : ThreadsCtxMenuCommand {
+		[ImportingConstructor]
+		SwitchToThreadNewTabThreadsCtxMenuCommand(Lazy<IThreadsContent> threadesContent)
+			: base(threadesContent) {
+		}
+
+		public override void Execute(ThreadsCtxMenuContext context) => context.Operations.SwitchToThread(newTab: true);
+		public override bool IsEnabled(ThreadsCtxMenuContext context) => context.Operations.CanSwitchToThread;
+	}
+
+	[ExportMenuItem(Header = "res:RenameThreadCommand", Group = MenuConstants.GROUP_CTX_DBG_THREADS_CMDS, Order = 20)]
+	sealed class RenameThreadCtxMenuCommand : ThreadsCtxMenuCommand {
+		[ImportingConstructor]
+		RenameThreadCtxMenuCommand(Lazy<IThreadsContent> threadesContent)
+			: base(threadesContent) {
+		}
+
+		public override void Execute(ThreadsCtxMenuContext context) => context.Operations.RenameThread();
+		public override bool IsEnabled(ThreadsCtxMenuContext context) => context.Operations.CanRenameThread;
+	}
+
+	[ExportMenuItem(Header = "res:FreezeThreadCommand", Group = MenuConstants.GROUP_CTX_DBG_THREADS_CMDS, Order = 30)]
+	sealed class FreezeThreadsCtxMenuCommand : ThreadsCtxMenuCommand {
+		[ImportingConstructor]
+		FreezeThreadsCtxMenuCommand(Lazy<IThreadsContent> threadesContent)
+			: base(threadesContent) {
+		}
+
+		public override void Execute(ThreadsCtxMenuContext context) => context.Operations.FreezeThread();
+		public override bool IsEnabled(ThreadsCtxMenuContext context) => context.Operations.CanFreezeThread;
+	}
+
+	[ExportMenuItem(Header = "res:ThawThreadCommand", Group = MenuConstants.GROUP_CTX_DBG_THREADS_CMDS, Order = 40)]
+	sealed class ThawThreadsCtxMenuCommand : ThreadsCtxMenuCommand {
+		[ImportingConstructor]
+		ThawThreadsCtxMenuCommand(Lazy<IThreadsContent> threadesContent)
+			: base(threadesContent) {
+		}
+
+		public override void Execute(ThreadsCtxMenuContext context) => context.Operations.ThawThread();
+		public override bool IsEnabled(ThreadsCtxMenuContext context) => context.Operations.CanThawThread;
+	}
+}
--- Extensions/dnSpy.Debugger/dnSpy.Debugger/ToolWindows/Threads/ThreadsOperations.cs
+++ Extensions/dnSpy.Debugger/dnSpy.Debugger/ToolWindows/Threads/ThreadsOperations.cs
@@ -17,13 +17,125 @@
     along with dnSpy.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel.Composition;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Windows;
+using dnSpy.Contracts.Debugger;
+using dnSpy.Contracts.Text;
 
 namespace dnSpy.Debugger.ToolWindows.Threads {
 	abstract class ThreadsOperations {
+		public abstract bool CanCopy { get; }
+		public abstract void Copy();
+		public abstract bool CanSelectAll { get; }
+		public abstract void SelectAll();
+		public abstract bool CanToggleUseHexadecimal { get; }
+		public abstract void ToggleUseHexadecimal();
+		public abstract bool UseHexadecimal { get; set; }
+		public abstract bool CanSwitchToThread { get; }
+		public abstract void SwitchToThread(bool newTab);
+		public abstract bool CanRenameThread { get; }
+		public abstract void RenameThread();
+		public abstract bool CanFreezeThread { get; }
+		public abstract void FreezeThread();
+		public abstract bool CanThawThread { get; }
+		public abstract void ThawThread();
 	}
 
 	[Export(typeof(ThreadsOperations))]
 	sealed class ThreadsOperationsImpl : ThreadsOperations {
+		readonly IThreadsVM threadesVM;
+		readonly DebuggerSettings debuggerSettings;
+
+		ObservableCollection<ThreadVM> AllItems => threadesVM.AllItems;
+		ObservableCollection<ThreadVM> SelectedItems => threadesVM.SelectedItems;
+		//TODO: This should be view order
+		IEnumerable<ThreadVM> SortedSelectedItems => SelectedItems.OrderBy(a => a.Order);
+
+		[ImportingConstructor]
+		ThreadsOperationsImpl(IThreadsVM threadesVM, DebuggerSettings debuggerSettings) {
+			this.threadesVM = threadesVM;
+			this.debuggerSettings = debuggerSettings;
+		}
+
+		public override bool CanCopy => SelectedItems.Count != 0;
+		public override void Copy() {
+			var output = new StringBuilderTextColorOutput();
+			foreach (var vm in SortedSelectedItems) {
+				var formatter = vm.Context.Formatter;
+				formatter.WriteImage(output, vm);
+				output.Write(BoxedTextColor.Text, "\t");
+				formatter.WriteId(output, vm.Thread);
+				output.Write(BoxedTextColor.Text, "\t");
+				formatter.WriteManagedId(output, vm.Thread);
+				output.Write(BoxedTextColor.Text, "\t");
+				formatter.WriteCategoryText(output, vm);
+				output.Write(BoxedTextColor.Text, "\t");
+				formatter.WriteName(output, vm);
+				output.Write(BoxedTextColor.Text, "\t");
+				formatter.WriteLocation(output, vm.Thread);
+				output.Write(BoxedTextColor.Text, "\t");
+				formatter.WritePriority(output, vm);
+				output.Write(BoxedTextColor.Text, "\t");
+				formatter.WriteAffinityMask(output, vm);
+				output.Write(BoxedTextColor.Text, "\t");
+				formatter.WriteSuspendedCount(output, vm.Thread);
+				output.Write(BoxedTextColor.Text, "\t");
+				formatter.WriteProcessName(output, vm.Thread);
+				output.Write(BoxedTextColor.Text, "\t");
+				formatter.WriteAppDomain(output, vm.Thread);
+				output.Write(BoxedTextColor.Text, "\t");
+				formatter.WriteState(output, vm.Thread);
+				output.WriteLine();
+			}
+			var s = output.ToString();
+			if (s.Length > 0) {
+				try {
+					Clipboard.SetText(s);
+				}
+				catch (ExternalException) { }
+			}
+		}
+
+		public override bool CanSelectAll => SelectedItems.Count != AllItems.Count;
+		public override void SelectAll() {
+			SelectedItems.Clear();
+			foreach (var vm in AllItems)
+				SelectedItems.Add(vm);
+		}
+
+		public override bool CanToggleUseHexadecimal => true;
+		public override void ToggleUseHexadecimal() => UseHexadecimal = !UseHexadecimal;
+		public override bool UseHexadecimal {
+			get => debuggerSettings.UseHexadecimal;
+			set => debuggerSettings.UseHexadecimal = value;
+		}
+
+		public override bool CanSwitchToThread => SelectedItems.Count == 1;
+		public override void SwitchToThread(bool newTab) {
+			//TODO:
+		}
+
+		public override bool CanRenameThread => SelectedItems.Count == 1;
+		public override void RenameThread() {
+			//TODO:
+		}
+
+		public override bool CanFreezeThread => SelectedItems.Count == 1 && SelectedItems[0].Thread.SuspendedCount == 0;
+		public override void FreezeThread() {
+			if (!CanFreezeThread)
+				return;
+			//TODO:
+		}
+
+		public override bool CanThawThread => SelectedItems.Count == 1 && SelectedItems[0].Thread.SuspendedCount != 0;
+		public override void ThawThread() {
+			if (!CanThawThread)
+				return;
+			//TODO:
+		}
 	}
 }
