603ffade5adfd9ae27a347d36a3aebac8e6042ee
de4dot <de4dot@gmail.com>
2017-03-08 22:32:40 +0100

    Update asm info version

--- dnSpy/dnSpy/Properties/AssemblyInfo.Shared.cs
+++ dnSpy/dnSpy/Properties/AssemblyInfo.Shared.cs
@@ -27,8 +27,8 @@ using System.Reflection;
 static class DnSpyAssemblyConstants {
 	// Update App.config whenever this value changes.
 	public const string ASSEMBLY_VERSION							= "3.0.1.0";
-	// This is shown in the title bar. 3 numbers are enough
-	public const string ASSEMBLY_INFORMATIONAL_VERSION				= "3.x.x";
+	// This is shown in the title bar
+	public const string ASSEMBLY_INFORMATIONAL_VERSION				= "vN.e.x.t";
 	public const string ASSEMBLY_FILE_VERSION						= ASSEMBLY_VERSION;
 }
 #pragma warning restore 0436
