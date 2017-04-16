5b30e4595369869559d1e6d404e28163dc09d877
de4dot <de4dot@gmail.com>
2017-04-16 13:03:03 +0200

    Reset hit count when user adds a hit count option

---
+++ Extensions/dnSpy.Debugger/dnSpy.Debugger/Breakpoints/Code/ResetHitCount.cs
@@ -0,0 +1,58 @@
+﻿/*
+    Copyright (C) 2014-2017 de4dot@gmail.com
+
+    This file is part of dnSpy
+
+    dnSpy is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    dnSpy is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with dnSpy.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using dnSpy.Contracts.Debugger;
+using dnSpy.Contracts.Debugger.Breakpoints.Code;
+
+namespace dnSpy.Debugger.Breakpoints.Code {
+	/// <summary>
+	/// Resets the hit count whenever the user adds a new hit count option. This matches VS behavior
+	/// </summary>
+	[ExportDbgManagerStartListener]
+	sealed class ResetHitCount : IDbgManagerStartListener {
+		readonly Lazy<DbgCodeBreakpointHitCountService> dbgCodeBreakpointHitCountService;
+
+		[ImportingConstructor]
+		ResetHitCount(DbgCodeBreakpointsService dbgCodeBreakpointsService, Lazy<DbgCodeBreakpointHitCountService> dbgCodeBreakpointHitCountService) {
+			this.dbgCodeBreakpointHitCountService = dbgCodeBreakpointHitCountService;
+			dbgCodeBreakpointsService.BreakpointsModified += DbgCodeBreakpointsService_BreakpointsModified;
+		}
+
+		void DbgCodeBreakpointsService_BreakpointsModified(object sender, DbgBreakpointsModifiedEventArgs e) {
+			List<DbgCodeBreakpoint> resetThese = null;
+			foreach (var info in e.Breakpoints) {
+				if (info.OldSettings.HitCount != null)
+					continue;
+				var breakpoint = info.Breakpoint;
+				if (breakpoint.Settings.HitCount == null)
+					continue;
+				if (resetThese == null)
+					resetThese = new List<DbgCodeBreakpoint>();
+				resetThese.Add(breakpoint);
+			}
+			if (resetThese != null)
+				dbgCodeBreakpointHitCountService.Value.Reset(resetThese.ToArray());
+		}
+
+		void IDbgManagerStartListener.OnStart(DbgManager dbgManager) { }
+	}
+}
