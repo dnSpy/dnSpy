e5ebd9b7395d4359388d04514a56834071e80c16
de4dot <de4dot@gmail.com>
2017-11-25 18:26:43 +0100

    Update title bar version

--- dnSpy/dnSpy/Properties/AssemblyInfo.Shared.cs
+++ dnSpy/dnSpy/Properties/AssemblyInfo.Shared.cs
@@ -28,7 +28,7 @@ static class DnSpyAssemblyConstants {
 	// Update App.config whenever this value changes.
 	public const string ASSEMBLY_VERSION							= "4.0.0.0";
 	// This is shown in the title bar
-	public const string ASSEMBLY_INFORMATIONAL_VERSION				= "v4.0.0";
+	public const string ASSEMBLY_INFORMATIONAL_VERSION				= "v4.x.x";
 	public const string ASSEMBLY_FILE_VERSION						= ASSEMBLY_VERSION;
 }
 #pragma warning restore 0436
