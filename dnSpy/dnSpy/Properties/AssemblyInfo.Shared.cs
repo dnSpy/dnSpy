8772b2c5654bdd4bb5f9731b0be7f62c15f2a70f
de4dot <de4dot@gmail.com>
2018-05-18 18:26:17 +0200

    Set new version

--- dnSpy/dnSpy/Properties/AssemblyInfo.Shared.cs
+++ dnSpy/dnSpy/Properties/AssemblyInfo.Shared.cs
@@ -26,9 +26,9 @@ using System.Reflection;
 
 static class DnSpyAssemblyConstants {
 	// Update App.config whenever this value changes.
-	public const string ASSEMBLY_VERSION							= "5.0.3.0";
+	public const string ASSEMBLY_VERSION							= "5.0.4.0";
 	// This is shown in the title bar
-	public const string ASSEMBLY_INFORMATIONAL_VERSION				= "v5.0.3";
+	public const string ASSEMBLY_INFORMATIONAL_VERSION				= "v5.0.4";
 	public const string ASSEMBLY_FILE_VERSION						= ASSEMBLY_VERSION;
 }
 #pragma warning restore 0436
