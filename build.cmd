a343742ff49326ebe3deba39f2d14555adc02187
de4dot <de4dot@gmail.com>
2019-05-30 19:00:14 +0200

    Update build scripts

--- build.cmd
+++ build.cmd
@@ -2,7 +2,6 @@
 
 REM TODO remove this when appveyor build image has .NET Core 3.0 installed
 powershell -NoLogo -NoProfile -File build.ps1 -ExecutionPolicy Bypass
-goto :EOF
 
 
 echo dotnet SDK version
@@ -14,8 +13,8 @@ REM dotnet publish -c Release -f netcoreapp3.0 -r win-x86 --self-contained
 REM dotnet publish -c Release -f netcoreapp3.0 -r win-x64 --self-contained
 
 echo Building .NET Framework x86 and x64
-msbuild -v:m -t:Restore || goto :error
-msbuild -v:m -t:Build -p:Configuration=Release -p:TargetFramework=net472 || goto :error
+msbuild -v:m -t:Restore -p:Configuration=Release -p:TargetFramework=net472 || goto :error
+msbuild -v:m -t:Build   -p:Configuration=Release -p:TargetFramework=net472 || goto :error
 
 echo Building .NET Core x86
 msbuild -v:m -t:Restore -p:Configuration=Release -p:TargetFramework=netcoreapp3.0 -p:RuntimeIdentifier=win-x86 -p:SelfContained=True || goto :error
--- build.ps1
+++ build.ps1
@@ -1,33 +1,6 @@
 ﻿Set-StrictMode -Version Latest
 $ErrorActionPreference = "Stop"
 
-$dotnetsdkZipFile = "dotnetsdk.zip"
-Write-Host "Downloading latest .NET Core Preview"
-Invoke-WebRequest "https://download.visualstudio.microsoft.com/download/pr/78836c06-166d-4145-ae7b-da5693e36665/431a2fd34af25742527bc5cafe4d8fae/dotnet-sdk-3.0.100-preview5-011568-win-x64.zip" -OutFile "$dotnetsdkZipFile"
-$tempPath = [System.IO.Path]::GetTempPath()
-$dotnetPath = "$tempPath" + [System.IO.Path]::GetRandomFileName()
-Write-Host "Extracting the zip file"
-Expand-Archive "$dotnetsdkZipFile" -DestinationPath "$dotnetPath" -Force
-del "$dotnetsdkZipFile"
-$dotnetExe = "$dotnetPath\dotnet.exe"
-
-#
-# Show the version in the log
-#
-Invoke-Expression '&"$dotnetExe" --version'
-if ($LastExitCode -ne 0) { exit 1 }
-
-#
-# Build it
-#
-Write-Host "Building (.NET Framework)"
-Invoke-Expression '&"$dotnetExe" build -c Release -f net472'
-if ($LastExitCode -ne 0) { exit 1 }
-
-Write-Host "Building (.NET Core x86)"
-Invoke-Expression '&"$dotnetExe" publish -c Release -f netcoreapp3.0 -r win-x86 --self-contained'
-if ($LastExitCode -ne 0) { exit 1 }
-
-Write-Host "Building (.NET Core x64)"
-Invoke-Expression '&"$dotnetExe" publish -c Release -f netcoreapp3.0 -r win-x64 --self-contained'
-if ($LastExitCode -ne 0) { exit 1 }
+Invoke-WebRequest -Uri 'https://dot.net/v1/dotnet-install.ps1' -UseBasicParsing -OutFile "$env:temp\dotnet-install.ps1"
+# Update global.json if this version number gets updated
+& $env:temp\dotnet-install.ps1 -Channel 3.0 -Version 3.0.100-preview5-011568 -InstallDir "$env:ProgramFiles\dotnet" -Architecture x64
