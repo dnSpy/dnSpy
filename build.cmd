9bf300e613b0d322df712d98fbe4c5bcccbfc894
de4dot <de4dot@gmail.com>
2019-06-02 19:37:15 +0200

    Update SCCompositionVersion

--- build.cmd
+++ build.cmd
@@ -12,15 +12,15 @@ REM dotnet build -c Release -f net472
 REM dotnet publish -c Release -f netcoreapp3.0 -r win-x86 --self-contained
 REM dotnet publish -c Release -f netcoreapp3.0 -r win-x64 --self-contained
 
-echo Building .NET Framework x86 and x64
+echo Building .NET Framework x86 and x64 binaries
 msbuild -v:m -t:Restore -p:Configuration=Release -p:TargetFramework=net472 || goto :error
 msbuild -v:m -t:Build   -p:Configuration=Release -p:TargetFramework=net472 || goto :error
 
-echo Building .NET Core x86
+echo Building .NET Core x86 binaries
 msbuild -v:m -t:Restore -p:Configuration=Release -p:TargetFramework=netcoreapp3.0 -p:RuntimeIdentifier=win-x86 -p:SelfContained=True || goto :error
 msbuild -v:m -t:Publish -p:Configuration=Release -p:TargetFramework=netcoreapp3.0 -p:RuntimeIdentifier=win-x86 -p:SelfContained=True || goto :error
 
-echo Building .NET Core x64
+echo Building .NET Core x64 binaries
 msbuild -v:m -t:Restore -p:Configuration=Release -p:TargetFramework=netcoreapp3.0 -p:RuntimeIdentifier=win-x64 -p:SelfContained=True || goto :error
 msbuild -v:m -t:Publish -p:Configuration=Release -p:TargetFramework=netcoreapp3.0 -p:RuntimeIdentifier=win-x64 -p:SelfContained=True || goto :error
 goto :EOF
