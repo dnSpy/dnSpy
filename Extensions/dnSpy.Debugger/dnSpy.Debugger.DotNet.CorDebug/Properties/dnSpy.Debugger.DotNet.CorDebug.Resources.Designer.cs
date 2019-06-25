b1ca34d78f6c86b9b9f8433335569a7ec73de8dc
de4dot <de4dot@gmail.com>
2019-06-25 21:44:09 +0200

    Change debug dialog label from Assembly to Executable

--- Extensions/dnSpy.Debugger/dnSpy.Debugger.DotNet.CorDebug/Properties/dnSpy.Debugger.DotNet.CorDebug.Resources.Designer.cs
+++ Extensions/dnSpy.Debugger/dnSpy.Debugger.DotNet.CorDebug/Properties/dnSpy.Debugger.DotNet.CorDebug.Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace dnSpy.Debugger.DotNet.CorDebug.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class dnSpy_Debugger_DotNet_CorDebug_Resources {
@@ -71,20 +71,20 @@ namespace dnSpy.Debugger.DotNet.CorDebug.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to _Assembly.
-        /// </summary>
-        public static string DbgAsm_Assembly {
-            get {
-                return ResourceManager.GetString("DbgAsm_Assembly", resourceCulture);
-            }
-        }
-        
-        /// <summary>
         ///   Looks up a localized string similar to _Break at.
         /// </summary>
         public static string DbgAsm_BreakAt {
             get {
                 return ResourceManager.GetString("DbgAsm_BreakAt", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to E_xecutable.
+        /// </summary>
+        public static string DbgAsm_Executable {
+            get {
+                return ResourceManager.GetString("DbgAsm_Executable", resourceCulture);
             }
         }
         
