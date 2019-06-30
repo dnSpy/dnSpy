c613f8c2c8df7b5ca96d35d9723e58a77a196573
de4dot <de4dot@gmail.com>
2019-06-30 14:42:08 +0200

    Attribute should be internal, not public

--- Extensions/dnSpy.Debugger/AppHostInfoGenerator/NRT.cs
+++ Extensions/dnSpy.Debugger/AppHostInfoGenerator/NRT.cs
@@ -1,8 +1,6 @@
 namespace System.Runtime.CompilerServices {
-#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
 	[AttributeUsage(AttributeTargets.Parameter, AllowMultiple = false)]
-	public sealed class NotNullWhenTrueAttribute : Attribute {
+	sealed class NotNullWhenTrueAttribute : Attribute {
 		public NotNullWhenTrueAttribute() { }
 	}
-#pragma warning restore CS1591 // Missing XML comment for publicly visible type or member
 }
