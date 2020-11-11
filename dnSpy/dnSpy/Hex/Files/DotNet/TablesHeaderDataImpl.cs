81dab08a4a82636ec8812a02c1490fb764ccd65f
cw2k <3834079+cw2k@users.noreply.github.com>
2020-11-11 21:07:13 +0100

    Update TablesHeaderDataImpl.cs

--- dnSpy/dnSpy/Hex/Files/DotNet/TablesHeaderDataImpl.cs
+++ dnSpy/dnSpy/Hex/Files/DotNet/TablesHeaderDataImpl.cs
@@ -25,113 +25,117 @@ using dnSpy.Contracts.Hex.Files.DotNet;
 
 namespace dnSpy.Hex.Files.DotNet {
 	sealed class TablesHeaderDataImpl : TablesHeaderData {
-		public override StructField<UInt32Data> Reserved { get; }
-		public override StructField<ByteData> MajorVersion { get; }
-		public override StructField<ByteData> MinorVersion { get; }
-		public override StructField<ByteFlagsData> Flags { get; }
-		public override StructField<ByteData> Log2Rid { get; }
-		public override StructField<UInt64FlagsData> ValidMask { get; }
+		public override StructField<UInt32Data> Reserved        { get; }
+		public override StructField<ByteData> MajorVersion      { get; }
+		public override StructField<ByteData> MinorVersion      { get; }
+		public override StructField<ByteFlagsData> Flags        { get; }
+		public override StructField<ByteData> Log2Rid           { get; }
+		public override StructField<UInt64FlagsData> ValidMask  { get; }
 		public override StructField<UInt64FlagsData> SortedMask { get; }
-		public override StructField<UInt32Data>? ExtraData { get; }
+		public override StructField<UInt32Data>? ExtraData      { get; }
 		public override StructField<ArrayData<UInt32Data>> Rows { get; }
 
 		protected override BufferField[] Fields { get; }
 
 		static readonly ReadOnlyCollection<FlagInfo> heapsFlagInfos = new ReadOnlyCollection<FlagInfo>(new FlagInfo[] {
 			new FlagInfo(0x01, "HEAP_STRING_4"),
-			new FlagInfo(0x02, "HEAP_GUID_4"),
-			new FlagInfo(0x04, "HEAP_BLOB_4"),
-			new FlagInfo(0x08, "PADDING_BIT"),
-			new FlagInfo(0x10, "RESERVED"),
-			new FlagInfo(0x20, "DELTA_ONLY"),
-			new FlagInfo(0x40, "EXTRA_DATA"),
-			new FlagInfo(0x80, "HAS_DELETE"),
+			new FlagInfo(0x02, "HEAP_GUID_4"  ),
+			new FlagInfo(0x04, "HEAP_BLOB_4"  ),
+			new FlagInfo(0x08, "PADDING_BIT"  ),
+			new FlagInfo(0x10, "RESERVED"     ),
+			new FlagInfo(0x20, "DELTA_ONLY"   ),
+			new FlagInfo(0x40, "EXTRA_DATA"   ),
+			new FlagInfo(0x80, "HAS_DELETE"   ),
 		});
 
 		internal static readonly ReadOnlyCollection<FlagInfo> tableFlagInfos = new ReadOnlyCollection<FlagInfo>(new FlagInfo[64] {
-			new FlagInfo(0x0000000000000001, "Module"),
-			new FlagInfo(0x0000000000000002, "TypeRef"),
-			new FlagInfo(0x0000000000000004, "TypeDef"),
-			new FlagInfo(0x0000000000000008, "FieldPtr"),
-			new FlagInfo(0x0000000000000010, "Field"),
-			new FlagInfo(0x0000000000000020, "MethodPtr"),
-			new FlagInfo(0x0000000000000040, "Method"),
-			new FlagInfo(0x0000000000000080, "ParamPtr"),
-			new FlagInfo(0x0000000000000100, "Param"),
-			new FlagInfo(0x0000000000000200, "InterfaceImpl"),
-			new FlagInfo(0x0000000000000400, "MemberRef"),
-			new FlagInfo(0x0000000000000800, "Constant"),
-			new FlagInfo(0x0000000000001000, "CustomAttribute"),
-			new FlagInfo(0x0000000000002000, "FieldMarshal"),
-			new FlagInfo(0x0000000000004000, "DeclSecurity"),
-			new FlagInfo(0x0000000000008000, "ClassLayout"),
-			new FlagInfo(0x0000000000010000, "FieldLayout"),
-			new FlagInfo(0x0000000000020000, "StandAloneSig"),
-			new FlagInfo(0x0000000000040000, "EventMap"),
-			new FlagInfo(0x0000000000080000, "EventPtr"),
-			new FlagInfo(0x0000000000100000, "Event"),
-			new FlagInfo(0x0000000000200000, "PropertyMap"),
-			new FlagInfo(0x0000000000400000, "PropertyPtr"),
-			new FlagInfo(0x0000000000800000, "Property"),
-			new FlagInfo(0x0000000001000000, "MethodSemantics"),
-			new FlagInfo(0x0000000002000000, "MethodImpl"),
-			new FlagInfo(0x0000000004000000, "ModuleRef"),
-			new FlagInfo(0x0000000008000000, "TypeSpec"),
-			new FlagInfo(0x0000000010000000, "ImplMap"),
-			new FlagInfo(0x0000000020000000, "FieldRVA"),
-			new FlagInfo(0x0000000040000000, "ENCLog"),
-			new FlagInfo(0x0000000080000000, "ENCMap"),
-			new FlagInfo(0x0000000100000000, "Assembly"),
-			new FlagInfo(0x0000000200000000, "AssemblyProcessor"),
-			new FlagInfo(0x0000000400000000, "AssemblyOS"),
-			new FlagInfo(0x0000000800000000, "AssemblyRef"),
-			new FlagInfo(0x0000001000000000, "AssemblyRefProcessor"),
-			new FlagInfo(0x0000002000000000, "AssemblyRefOS"),
-			new FlagInfo(0x0000004000000000, "File"),
-			new FlagInfo(0x0000008000000000, "ExportedType"),
-			new FlagInfo(0x0000010000000000, "ManifestResource"),
-			new FlagInfo(0x0000020000000000, "NestedClass"),
-			new FlagInfo(0x0000040000000000, "GenericParam"),
-			new FlagInfo(0x0000080000000000, "MethodSpec"),
-			new FlagInfo(0x0000100000000000, "GenericParamConstraint"),
-			new FlagInfo(0x0000200000000000, "Reserved2D"),
-			new FlagInfo(0x0000400000000000, "Reserved2E"),
-			new FlagInfo(0x0000800000000000, "Reserved2F"),
-			new FlagInfo(0x0001000000000000, "Document"),
-			new FlagInfo(0x0002000000000000, "MethodDebugInformation"),
-			new FlagInfo(0x0004000000000000, "LocalScope"),
-			new FlagInfo(0x0008000000000000, "LocalVariable"),
-			new FlagInfo(0x0010000000000000, "LocalConstant"),
-			new FlagInfo(0x0020000000000000, "ImportScope"),
-			new FlagInfo(0x0040000000000000, "StateMachineMethod"),
-			new FlagInfo(0x0080000000000000, "CustomDebugInformation"),
-			new FlagInfo(0x0100000000000000, "Reserved38"),
-			new FlagInfo(0x0200000000000000, "Reserved39"),
-			new FlagInfo(0x0400000000000000, "Reserved3A"),
-			new FlagInfo(0x0800000000000000, "Reserved3B"),
-			new FlagInfo(0x1000000000000000, "Reserved3C"),
-			new FlagInfo(0x2000000000000000, "Reserved3D"),
-			new FlagInfo(0x4000000000000000, "Reserved3E"),
-			new FlagInfo(0x8000000000000000, "Reserved3F"),
+			new FlagInfo(0x0000000000000001, "00_Module"                ),
+			new FlagInfo(0x0000000000000002, "01_TypeRef"               ),
+			new FlagInfo(0x0000000000000004, "02_TypeDef"               ),
+			new FlagInfo(0x0000000000000008, "03_FieldPtr"              ),
+			new FlagInfo(0x0000000000000010, "04_Field"                 ),
+			new FlagInfo(0x0000000000000020, "05_MethodPtr"             ),
+			new FlagInfo(0x0000000000000040, "06_Method"                ),
+			new FlagInfo(0x0000000000000080, "07_ParamPtr"              ),
+			new FlagInfo(0x0000000000000100, "08_Param"                 ),
+			new FlagInfo(0x0000000000000200, "09_InterfaceImpl"         ),
+			new FlagInfo(0x0000000000000400, "0A_MemberRef"             ),
+			new FlagInfo(0x0000000000000800, "0B_Constant"              ),
+			new FlagInfo(0x0000000000001000, "0C_CustomAttribute"       ),
+			new FlagInfo(0x0000000000002000, "0D_FieldMarshal"          ),
+			new FlagInfo(0x0000000000004000, "0E_DeclSecurity"          ),
+			new FlagInfo(0x0000000000008000, "0F_ClassLayout"           ),
+			new FlagInfo(0x0000000000010000, "10_FieldLayout"           ),
+			new FlagInfo(0x0000000000020000, "11_StandAloneSig"         ),
+			new FlagInfo(0x0000000000040000, "12_EventMap"              ),
+			new FlagInfo(0x0000000000080000, "13_EventPtr"              ),
+			new FlagInfo(0x0000000000100000, "14_Event"                 ),
+			new FlagInfo(0x0000000000200000, "15_PropertyMap"           ),
+			new FlagInfo(0x0000000000400000, "16_PropertyPtr"           ),
+			new FlagInfo(0x0000000000800000, "17_Property"              ),
+			new FlagInfo(0x0000000001000000, "18_MethodSemantics"       ),
+			new FlagInfo(0x0000000002000000, "19_MethodImpl"            ),
+			new FlagInfo(0x0000000004000000, "1A_ModuleRef"             ),
+			new FlagInfo(0x0000000008000000, "1B_TypeSpec"              ),
+			new FlagInfo(0x0000000010000000, "1C_ImplMap"               ),
+			new FlagInfo(0x0000000020000000, "1D_FieldRVA"              ),
+			new FlagInfo(0x0000000040000000, "1E_EnCLog"                ),
+			new FlagInfo(0x0000000080000000, "1F_EnCMap"                ),
+			new FlagInfo(0x0000000100000000, "20_Assembly"              ),
+			new FlagInfo(0x0000000200000000, "21_AssemblyProcessor"     ),
+			new FlagInfo(0x0000000400000000, "22_AssemblyOS"            ),
+			new FlagInfo(0x0000000800000000, "23_AssemblyRef"           ),
+			new FlagInfo(0x0000001000000000, "24_AssemblyRefProcessor"  ),
+			new FlagInfo(0x0000002000000000, "25_AssemblyRefOS"         ),
+			new FlagInfo(0x0000004000000000, "26_File"                  ),
+			new FlagInfo(0x0000008000000000, "27_ExportedType"          ),
+			new FlagInfo(0x0000010000000000, "28_ManifestResource"      ),
+			new FlagInfo(0x0000020000000000, "29_NestedClass"           ),
+			new FlagInfo(0x0000040000000000, "2A_GenericParam"          ),
+			new FlagInfo(0x0000080000000000, "2B_MethodSpec"            ),
+			new FlagInfo(0x0000100000000000, "2C_GenericParamConstraint"),
+			new FlagInfo(0x0000200000000000, "2D_Reserved"              ),
+			new FlagInfo(0x0000400000000000, "2E_Reserved"              ),
+			new FlagInfo(0x0000800000000000, "2F_Reserved"              ),
+			new FlagInfo(0x0001000000000000, "30_Document"              ),
+			new FlagInfo(0x0002000000000000, "31_MethodDebugInformation"),
+			new FlagInfo(0x0004000000000000, "32_LocalScope"            ),
+			new FlagInfo(0x0008000000000000, "33_LocalVariable"         ),
+			new FlagInfo(0x0010000000000000, "34_LocalConstant"         ),
+			new FlagInfo(0x0020000000000000, "35_ImportScope"           ),
+			new FlagInfo(0x0040000000000000, "36_StateMachineMethod"    ),
+			new FlagInfo(0x0080000000000000, "37_CustomDebugInformation"),
+			new FlagInfo(0x0100000000000000, "38_Reserved"              ),
+			new FlagInfo(0x0200000000000000, "39_Reserved"              ),
+			new FlagInfo(0x0400000000000000, "3A_Reserved"              ),
+			new FlagInfo(0x0800000000000000, "3B_Reserved"              ),
+			new FlagInfo(0x1000000000000000, "3C_Reserved"              ),
+			new FlagInfo(0x2000000000000000, "3D_Reserved"              ),
+			new FlagInfo(0x4000000000000000, "3E_Reserved"              ),
+			new FlagInfo(0x8000000000000000, "3F_Reserved"              ),
 		});
 
 		public TablesHeaderDataImpl(HexBufferSpan span, bool hasExtraData, int rowsFieldCount)
 			: base(span) {
 			var buffer = span.Buffer;
-			var pos = span.Start.Position;
-			Reserved = new StructField<UInt32Data>("m_ulReserved", new UInt32Data(buffer, pos));
-			MajorVersion = new StructField<ByteData>("m_major", new ByteData(buffer, pos + 4));
-			MinorVersion = new StructField<ByteData>("m_minor", new ByteData(buffer, pos + 5));
-			Flags = new StructField<ByteFlagsData>("m_heaps", new ByteFlagsData(buffer, pos + 6, heapsFlagInfos));
-			Log2Rid = new StructField<ByteData>("m_rid", new ByteData(buffer, pos + 7));
-			ValidMask = new StructField<UInt64FlagsData>("m_maskvalid", new UInt64FlagsData(buffer, pos + 8, tableFlagInfos));
-			SortedMask = new StructField<UInt64FlagsData>("m_sorted", new UInt64FlagsData(buffer, pos + 0x10, tableFlagInfos));
+			var pos    = span.Start.Position;
+
+			Reserved      = new StructField<UInt32Data>     ( "Reserved"  , new UInt32Data     (buffer, pos       ));
+			MajorVersion  = new StructField<ByteData>       ( "MajorVer"  , new ByteData       (buffer, pos +    4));
+			MinorVersion  = new StructField<ByteData>       ( "MinorVer"  , new ByteData       (buffer, pos +    5));
+			Flags         = new StructField<ByteFlagsData>  ( "HeapSizes" , new ByteFlagsData  (buffer, pos +    6, heapsFlagInfos)); // a bitmask-byte that encodes how wide(=16 or 32 bit) offsets into the various heaps are
+			Log2Rid       = new StructField<ByteData>       ( "Rid"       , new ByteData       (buffer, pos +    7));                 // Reserved Byte, always 1
+			ValidMask     = new StructField<UInt64FlagsData>( "MaskValid" , new UInt64FlagsData(buffer, pos +    8, tableFlagInfos)); // a bitmask-qword telling which MetaData tables are present in the assembly.
+			SortedMask    = new StructField<UInt64FlagsData>( "MaskSorted", new UInt64FlagsData(buffer, pos + 0x10, tableFlagInfos)); // a bitmask-qword telling which MetaData tables  are sorted.
 			pos += 0x18;
-			Rows = new StructField<ArrayData<UInt32Data>>("m_rows", ArrayData.CreateUInt32Array(buffer, pos, rowsFieldCount));
+
+			Rows          = new StructField<ArrayData<UInt32Data>>("RowsCounts" , ArrayData.CreateUInt32Array(buffer, pos, rowsFieldCount));
+
 			if (hasExtraData) {
-				ExtraData = new StructField<UInt32Data>("m_ulExtra", new UInt32Data(buffer, pos));
+				ExtraData = new StructField<UInt32Data>(      "Extra"   , new UInt32Data(buffer, pos));
 				pos += 4;
 			}
+
 			var fields = new List<BufferField>(9) {
 				Reserved,
 				MajorVersion,
@@ -142,6 +146,7 @@ namespace dnSpy.Hex.Files.DotNet {
 				SortedMask,
 			};
 			fields.Add(Rows);
+
 			if (hasExtraData)
 				fields.Add(ExtraData!);
 			Fields = fields.ToArray();
